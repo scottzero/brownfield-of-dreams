e1ace8ed99d9e39a4fbdc60c57f6cd3ebe830b71
Scott Payton <spayton.c@gmail.com>
2019-10-07 15:03:00 -0600

    added
    Co-Authored-By: evettetelyas <evettetelyas@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tutorials, only:[:show, :index]
       resources :videos, only:[:show]
-      #commentn test
+      #commentn testxxx
     end
   end
 
