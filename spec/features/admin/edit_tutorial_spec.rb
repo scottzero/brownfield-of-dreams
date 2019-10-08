e62df73f0ea0784bc9602b5393df27f180bbcaa2
Evette Telyas <etelyas@gmail.com>
2019-10-07 21:39:26 -0600

    remove comments

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -19,11 +19,9 @@ describe "An Admin can edit a tutorial" do
 
       expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
 
-      # within("#video-list") do 
-        within(first(".video")) do
-          expect(page).to have_content("How to tie your shoes.")
-        end
-      # end
+      within(first(".video")) do
+        expect(page).to have_content("How to tie your shoes.")
+      end
     end
   end
 end
