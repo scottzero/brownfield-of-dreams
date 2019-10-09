8c966b72192734bb0cfbe45bd07909a9c255679d
Evette Telyas <etelyas@gmail.com>
2019-10-09 15:08:41 -0600

    oauth for github followers/following Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
-  validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates :email, uniqueness: true, presence: true, if: :no_github_token?
+  validates :password, presence: true, allow_nil: true
   validates_presence_of :first_name
   enum role: { default: 0, admin: 1 }
   has_secure_password
+
+  def no_github_token?
+    !self.github_token
+  end
+
+  def update_github(token, uid)
+    self.github_token = token
+    self.uid = uid
+    self.save
+  end
 end
