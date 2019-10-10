055c1866f510cb5b3c37385bf3a779a3074f8b63
Evette Telyas <etelyas@gmail.com>
2019-10-10 12:56:16 -0600

    fix password bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to dashboard_path
     else
-      flash[:error] = "Username already exists"
+      flash[:error] = @user.errors.full_messages.uniq.to_sentence
       render :new
     end
   end
