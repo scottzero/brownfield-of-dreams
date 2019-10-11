18bb5970664e4f7984cbfdc277a46f0dccc8124d
Evette Telyas <etelyas@gmail.com>
2019-10-11 11:44:32 -0600

    update video factory to include tutorial

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     title { Faker::Pokemon.name }
     description { Faker::SiliconValley.motto }
     video_id { Faker::Crypto.md5 }
+    thumbnail { Faker::Pokemon.name + ".jpg" }
     tutorial
   end
 end
