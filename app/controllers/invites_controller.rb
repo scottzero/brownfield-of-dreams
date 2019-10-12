4c3df6a210f3589b84f9b06fd5b86ccbf1eb9e15
Evette Telyas <etelyas@gmail.com>
2019-10-12 08:35:51 -0600

    refactor gh handle mailer Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,10 +3,10 @@ class InvitesController < ApplicationController
     invitee = InvitesFacade.new(params[:github_handle])
     email = invitee.email
     name = invitee.name
-    GithubInviteMailer.invite(current_user, email, name).deliver_now
     if !email
       flash[:notice] = "No email listed for this user"
     else
+      GithubInviteMailer.invite(current_user, email, name).deliver_now
       flash[:notice] = "Invite sent!"
     end
     redirect_to dashboard_path
