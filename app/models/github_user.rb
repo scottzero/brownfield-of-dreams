5bd595c4c32dbadd2aa07a6e2316b485611437d6
Evette Telyas <etelyas@gmail.com>
2019-10-12 17:49:22 -0600

    friendships Co-authored-by: Scott Payton <33855435+wolfeJob@users.noreply.github.com>

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,8 +1,22 @@
 class GithubUser
-  attr_reader :name, :url
+  attr_reader :name, :url, :uid
 
   def initialize(data)
     @name = data[:login]
     @url = data[:html_url]
+    @uid = data[:id].to_s
+  end
+
+  def registered?
+    User.any? { |user| user.uid == @uid }
+  end
+
+  def user
+    User.find_by(uid: @uid)
+  end
+
+  def friends?(friend)
+    (Friendship.where(user_id: self.user.id, friend_id: friend.id)).count > 0 ||
+    (Friendship.where(friend_id: self.user.id, user_id: friend.id)).count > 0
   end
 end
