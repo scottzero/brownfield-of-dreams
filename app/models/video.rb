7fc918d50bc68aab040766188fdcdcf13cd4b9cb
Scott Payton <spayton.c@gmail.com>
2019-10-12 04:16:29 -0600

    add position validation in videos model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,6 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial, inverse_of: :videos
+
+  validates_presence_of :position, numericality: { greater_than_or_equal_to: 0 }
 end
