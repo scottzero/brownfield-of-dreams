9694a1b15e3b9f15c172874931541c1fc304f4d9
Evette Telyas <etelyas@gmail.com>
2019-10-15 16:55:31 -0600

    update specs

--- spec/features/admin/admin_add_tutorial_spec.rb
+++ spec/features/admin/admin_add_tutorial_spec.rb
@@ -10,12 +10,22 @@ xdescribe "Admin creates a new tutorial" do
 
 		click_on "New Tutorial"
 
-		fill_in "tutorial[title]", with: "Learning to do stuff"
-		fill_in "tutorial[description]", with: "stuff you learn to do"
-		fill_in "tutorial[thumbnail]", with: "img.jpg"
-		fill_in "videos_attributes_0_first_name", with: "How to tie your shoes."
-		fill_in "videos_attributes_0_description", with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
-		fill_in "videos_attributes_0_thumbnail", with: "img.jpg"
-		fill_in "videos_attributes_0_video_id", with: "J7ikFUlkP_k"
+		within(".new-tutorials") do
+			fill_in "Tutorial Title", with: "Learning to do stuff"
+			fill_in "Tutorial Description", with: "stuff you learn to do"
+		end
+
+		within(".new-videos") do
+			fill_in "Title", with: "How to tie your shoes."
+			fill_in "Description", with: "Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through."
+			fill_in "YouTube ID", with: "J7ikFUlkP_k"
+		end
+
+		click_button "Save"
+
+		tutorial = Tutorial.last
+
+		expect(page).to have_content("Tutorial successfully created!")
+		expect(current_path).to eq(tutorial_path(tutorial))
 	end
 end
