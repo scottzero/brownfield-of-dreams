e7e84c90c17fbc21185e403fa2aa71160cbe2acf
Evette Telyas <etelyas@gmail.com>
2019-10-16 16:37:39 -0600

    rubocop

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::BookmarksController < ApplicationController
-	skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-	def create
-		bookmark = UserVideo.create(bookmark_params)
-		render json: bookmark
-	end
+  def create
+    bookmark = UserVideo.create(bookmark_params)
+    render json: bookmark
+  end
 
-	private
-	def bookmark_params
-		params.require(:bookmark).permit(:user_id, :video_id)
-	end
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:user_id, :video_id)
+    end
   end
