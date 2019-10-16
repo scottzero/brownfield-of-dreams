91684e09860c8731920b1a04380bb22b5d98fab6
Evette Telyas <etelyas@gmail.com>
2019-10-15 19:37:08 -0600

    rubocop happy

--- app/controllers/admin/playlists_controller.rb
+++ app/controllers/admin/playlists_controller.rb
@@ -1,27 +1,25 @@
 class Admin::PlaylistsController < Admin::BaseController
-	def new
-	end
+  def new
+  end
 
-	def create
-		begin
-			@playlist = YouTube::Playlist.by_id(playlist_params[:youtube_id])
-			tutorial = Tutorial.new(@playlist.instance_values)
-			if tutorial.save
-				@playlist.create_videos(tutorial)
-				flash[:notice] = "Tutorial created!"
-				redirect_to tutorial_path(tutorial)
-			else
-				flash[:notice] = tutorial.errors.full_messages.to_sentence
-				render :new
-			end
-		rescue
-			flash[:notice] = "Invalid Playlist ID"
-			render :new
-		end
-	end
+  def create
+    @playlist = YouTube::Playlist.by_id(playlist_params[:youtube_id])
+    tutorial = Tutorial.new(@playlist.instance_values)
+    if tutorial.save
+      @playlist.create_videos(tutorial)
+      flash[:notice] = "Tutorial created!"
+      redirect_to tutorial_path(tutorial)
+    else
+      flash[:notice] = tutorial.errors.full_messages.to_sentence
+      render :new
+    end
+  rescue
+    flash[:notice] = "Invalid Playlist ID"
+    render :new
+  end
 
-	private
-	def playlist_params
-		params.require(:playlist).permit(:youtube_id)
-	end
+  private
+    def playlist_params
+      params.require(:playlist).permit(:youtube_id)
+    end
 end
--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -10,21 +10,19 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
-    begin
-      thumbnail = YouTube::Video.by_id(tutorial_params[:videos_attributes]["0"][:video_id]).thumbnail
-      @tutorial = Tutorial.new(tutorial_params.merge(thumbnail: thumbnail))
-      @tutorial.videos.first.update(thumbnail: thumbnail)
-      if @tutorial.save && @tutorial.videos.first.save
-        flash[:success] = "tutorial successfully created!"
-        redirect_to tutorial_path(@tutorial)
-      else
-        flash[:error] = (@tutorial.errors.full_messages).uniq.to_sentence
-        render :new
-      end
-    rescue
-      flash[:error] = "Invalid YouTube Video ID"
+    thumbnail = YouTube::Video.by_id(tutorial_params[:videos_attributes]["0"][:video_id]).thumbnail
+    @tutorial = Tutorial.new(tutorial_params.merge(thumbnail: thumbnail))
+    @tutorial.videos.first.update(thumbnail: thumbnail)
+    if @tutorial.save && @tutorial.videos.first.save
+      flash[:success] = "tutorial successfully created!"
+      redirect_to tutorial_path(@tutorial)
+    else
+      flash[:error] = (@tutorial.errors.full_messages).uniq.to_sentence
       render :new
     end
+  rescue
+    flash[:error] = "Invalid YouTube Video ID"
+    render :new
   end
 
   def new
